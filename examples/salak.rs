e7ae946e42399e29a8d7240f1c8f03416f171276
Daniel YU <i@icymint.me>
2021-08-22 08:51:44 +0800

    add examples

---
+++ examples/salak.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+use cfg_rs::*;
+
+#[derive(FromConfig, Debug)]
+#[config(prefix = "salak")]
+struct Config {
+    #[config(default = "world")]
+    hello: String,
+    world: Option<String>,
+    #[config(name = "hello")]
+    hey: Option<String>,
+    #[config(default = 123)]
+    num: u8,
+    arr: Vec<u8>,
+    map: HashMap<String, u8>,
+}
+
+fn main() -> Result<(), ConfigError> {
+    let env = Configuration::init()?;
+    for _i in 0..1000 {
+        let c = env.get_predefined::<Config>()?;
+        if _i % 100 == 0 {
+            println!("Round {}: {} - {}", _i, c.hello, c.num);
+        }
+    }
+    Ok(())
+}
---
+++ examples/thread_pool.rs
@@ -0,0 +1,70 @@
+use std::{path::PathBuf, time::Duration};
+
+use cfg_rs::*;
+
+/// Generic Pool Configuration.
+#[derive(FromConfig, Debug)]
+pub struct PoolConfig {
+    #[config(default = "${pool.max_size:5}")]
+    max_size: Option<u32>,
+    #[config(default = "${pool.min_idle:1}")]
+    min_idle: Option<u32>,
+    #[config(default = "${pool.thread_name:}")]
+    thread_name: Option<String>,
+    #[config(default = "${pool.thread_nums:}")]
+    thread_nums: Option<usize>,
+    #[config(default = "${pool.test_on_check_out:}")]
+    test_on_check_out: Option<bool>,
+    #[config(default = "${pool.max_lifetime:}")]
+    max_lifetime: Option<Duration>,
+    #[config(default = "${pool.idle_timeout:}")]
+    idle_timeout: Option<Duration>,
+    #[config(default = "${pool.connection_timeout:1s}")]
+    connection_timeout: Option<Duration>,
+    #[config(default = "${pool.wait_for_init:false}")]
+    wait_for_init: bool,
+}
+
+#[derive(FromConfig, Debug)]
+#[config(prefix = "postgresql")]
+pub struct PostgresConfig {
+    host: Vec<String>,
+    port: Option<u16>,
+    #[config(default = "postgres")]
+    user: String,
+    password: Option<String>,
+    dbname: Option<String>,
+    options: Option<String>,
+    #[config(default = "${app.name}")]
+    application_name: Option<String>,
+    #[config(default = "500ms")]
+    connect_timeout: Option<Duration>,
+    keepalives: Option<bool>,
+    keepalives_idle: Option<Duration>,
+    #[config(default = "true")]
+    must_allow_write: bool,
+    ssl: Option<PostgresSslConfig>,
+    pool: PoolConfig,
+}
+
+#[derive(FromConfig, Debug)]
+pub struct PostgresSslConfig {
+    cert_path: PathBuf,
+}
+
+fn main() -> Result<(), ConfigError> {
+    let config = Configuration::builder()
+        .set("postgresql.application_name", "primary")
+        .set("postgresql.secondary.application_name", "secondary")
+        .init()?;
+
+    // Equal to key "postgresql".
+    let pool = config.get_predefined::<PostgresConfig>()?;
+    assert_eq!(Some("primary"), pool.application_name.as_deref());
+    println!("{:?}", pool);
+
+    let pool2 = config.get::<PostgresConfig>("postgresql.secondary")?;
+    assert_eq!(Some("secondary"), pool2.application_name.as_deref());
+    println!("{:?}", pool2);
+    Ok(())
+}
