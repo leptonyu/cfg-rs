bbd4e3d559f9fe5db558b0a906ae9b0d68e4efd8
Daniel YU <i@icymint.me>
2021-08-29 10:03:36 +0800

    key test - 4

--- src/configuration.rs
+++ src/configuration.rs
@@ -670,8 +670,28 @@ mod test {
 
     #[test]
     fn get_test() {
-        let config = build_config();
+        let config = build_config()
+            .register_kv("k1")
+            .set("key[0]", "xx")
+            .finish()
+            .unwrap()
+            .register_kv("k2")
+            .set("key[5]", "xx")
+            .finish()
+            .unwrap();
         assert_eq!(1, config.get_or("a1", 1).unwrap());
+        let v = config.get::<Vec<Option<String>>>("key").unwrap();
+        assert_eq!(
+            vec![
+                Some("xx".to_string()),
+                None,
+                None,
+                None,
+                None,
+                Some("xx".to_string())
+            ],
+            v
+        );
     }
 
     #[test]
